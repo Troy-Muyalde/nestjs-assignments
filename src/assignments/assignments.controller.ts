e784dc751dbd1d54930a21dc3fc84117b7e5608e
Troy Muyalde <troydmuyalde@gmail.com>
2025-01-30 22:04:10 +0800

    Completed Assignment 2: Prime Number

--- src/assignments/assignments.controller.ts
+++ src/assignments/assignments.controller.ts
@@ -23,4 +23,21 @@ export class AssignmentsController {
     }
     return sequence.slice(0, n);
   }
+
+  @Get('prime/:number')
+  checkPrime(@Param('number') number: string): { isPrime: boolean } {
+    const num = parseInt(number, 10);
+    const isPrime = this.isPrime(num);
+    return { isPrime };
+  }
+
+  private isPrime(num: number): boolean {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+    for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+    return true;
+  }
 }
